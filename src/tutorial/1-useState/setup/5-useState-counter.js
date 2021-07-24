b99e80d219adeb76bf1a7ee52b0b42802de3dea9
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-24 15:11:32 -0400

    Update 5-useState-counter.js

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+// useState() is a higher-order function /hook that use a state
 const UseStateCounter = () => {
   return <h2>useState counter example</h2>;
 };
