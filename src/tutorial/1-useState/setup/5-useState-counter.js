9c402f6d5088a18cdfbd010add3c2e06bfa1af0a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-28 23:00:26 -0400

    Update 5-useState-counter.js

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // useState() is a higher-order function /hook that use a state
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  //  so we set up our state value 
+  const[ value, setValue] = useState(0);
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        {/* heading h1 we just target our value */}
+        <h1>{value}</h1>
+        {/* we're going to setup 3 buttons increase, decrease, reset */}
+        <button className="btn">Decrease</button>
+        <button className="btn">Increase</button>
+        <button className="btn">Reset</button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
